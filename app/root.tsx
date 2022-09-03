621a7bc1699bb2d766160535dffa5b15d42e4d9e
Arjen Bloemsma <arjen@bloemium.com>
2022-09-03 18:26:38 +0200

    Chore: Add remix (#12)
    * feat: :package: Added Remix
    
    * feat: :sparkles: Remix now renders same index.html
    
    * build: :lipstick: Added Tailwind CSS

---
+++ app/root.tsx
@@ -0,0 +1,43 @@
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import mainStyle from "./main.css";
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "Bloemium",
+  description: "bloemium software development services",
+  viewport: "width=device-width,initial-scale=1",
+  keywords:
+    "bloemium, software, development, typescript, javascript, frontend, react, remix, full-stack, cloud, azure, o365, office 365, sharepoint, sharepoint online, spo",
+});
+
+export const links: LinksFunction = () => {
+  return [
+    { rel: "canonical", href: "https://bloemium.com" },
+    { rel: "stylesheet", href: mainStyle },
+  ];
+};
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body className="bg-neutral-900">
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
