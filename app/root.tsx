ad5590833884f125c50639c8067373091e12e5f0
Arjen Bloemsma <arjen@bloemium.com>
2022-09-04 17:12:49 +0200

    Changed folder structure, file location, code formatting (#18)
    * build: :green_heart: Use different docker file per env
    
    * fix: :green_heart: Staging now uses 'remix dev'
    
    * fix: :green_heart: Shoud create staging build
    
    * fix: :green_heart: Removed watch from staging build
    
    * chore: Change structure of sources
    
    * build:
    
    * chore: :fire: Removed obsolete index.html
    
    * style: :art: Executed Prettier

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import type {LinksFunction, MetaFunction} from '@remix-run/node'
 import {
   Links,
   LiveReload,
@@ -6,25 +6,28 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
-import mainStyle from "./main.css";
+} from '@remix-run/react'
+import mainStyle from './styles/main.css'
 
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "Bloemium",
-  description: "bloemium software development services",
-  viewport: "width=device-width,initial-scale=1",
+  charset: 'utf-8',
+  title: 'Bloemium',
+  description: 'bloemium software development services',
+  viewport: 'width=device-width,initial-scale=1',
   keywords:
-    "bloemium, software, development, typescript, javascript, frontend, react, remix, full-stack, cloud, azure, o365, office 365, sharepoint, sharepoint online, spo",
-});
+    'bloemium, software, development, typescript, javascript, frontend, react, remix, full-stack, cloud, azure, o365, office 365, sharepoint, sharepoint online, spo',
+})
 
 export const links: LinksFunction = () => {
   return [
-    { rel: "canonical", href: "https://bloemium.com" },
-    { rel: "stylesheet", href: mainStyle },
-    { rel: "icon", href: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌸</text></svg>" },
-  ];
-};
+    {rel: 'canonical', href: 'https://bloemium.com'},
+    {rel: 'stylesheet', href: mainStyle},
+    {
+      rel: 'icon',
+      href: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌸</text></svg>',
+    },
+  ]
+}
 
 export default function App() {
   return (
@@ -40,5 +43,5 @@ export default function App() {
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
