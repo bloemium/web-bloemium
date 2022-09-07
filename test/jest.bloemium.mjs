06e7cf5ab6d2f358cd80da5a36db5c4a814bc127
Arjen Bloemsma <arjen@bloemium.com>
2022-09-07 18:12:57 +0200

    fix: :wrench: Fixed wrong glob

--- test/jest.bloemium.mjs
+++ test/jest.bloemium.mjs
@@ -2,6 +2,6 @@ import jestCommon from './jest.common.mjs'
 
 export default {
   ...jestCommon,
-  displayName: 'ingtoday',
-  testMatch: ['**/app/__tests__/**/*.ts'],
+  displayName: 'bloemium',
+  testMatch: ['<rootDir>/app/**/__tests__/**/*.ts'],
 }
